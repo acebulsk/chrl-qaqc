bbf1f14fc8e1b7d854c0b1f7838dec0fdaad9beb
Alex Cebulski <alexcebulski@gmail.com>
2024-04-02 19:46:40 -0600

    minor changes

--- scripts/summary-plots/05-plot-hourly-stats.R
+++ scripts/summary-plots/05-plot-hourly-stats.R
@@ -1,22 +1,151 @@
+sel_var <- "PC_accumulated_wtr_yr"
+sel_var <- "Air_Temp"
 
 color_palette <- c(viridisLite::viridis(n = (max_year - min_year),
                                         option = 'D'),'black')
 
-# now add in mean and cis
-ggplot(qc_fill_data, aes(plot_time, Air_Temp))  +
+file_path <- paste0('data/qaqc_chrl_w_ac_pc/qaqc_', cur_stn, '.rds')
+
+hourly_df <- readRDS(file_path) |> 
+  select(datetime, where(is.numeric)) |> 
+  pivot_longer(!datetime) |> 
+  filter(name == sel_var)|> 
+  mutate(year = format(datetime, '%Y'),
+         wtr_year = weatherdash::wtr_yr(datetime))
+
+hourly_df$plot_time <- format(hourly_df$datetime, "1900-%m-%d %H:%M:%S") # 81 so not a leap year
+hourly_df$plot_time <- as.POSIXct(hourly_df$plot_time, format = "%Y-%m-%d %H:%M:%S", tz = 'UTC')
+hourly_df$plot_time <- if_else(month(hourly_df$plot_time) < 10,
+                               weatherdash::set_yr(hourly_df$plot_time, 1901),
+                               weatherdash::set_yr(hourly_df$plot_time, 1900))
+
+file_path <- paste0(
+  'data/hourly_stats/',
+  cur_stn,
+  '_hourly_stats.rds'
+)
+hourly_stats <- readRDS(file_path) |> 
+  filter(name == sel_var)# |> 
+
+hourly_stats$plot_time <- if_else(month(hourly_stats$plot_time) < 10,
+                                  weatherdash::set_yr(hourly_stats$plot_time, 1901),
+                                  weatherdash::set_yr(hourly_stats$plot_time, 1900))
+
+
+ggplot(hourly_df)  +
   geom_line(aes(
+    plot_time, value,
+    colour = as.factor(wtr_year),
+    group = as.factor(wtr_year),
+  ))+
+  geom_ribbon(
+    data = hourly_stats,
+    aes(
+      x = plot_time,
+      ymin = min,
+      ymax = max,
+      fill = 'Max to Min Range'
+    ),
+    alpha = 0.2,
+    fill = 'dodgerblue',
+    colour = 'blue'
+  ) +
+  scale_color_viridis_d(name = 'Water Year') +  # Assign colors based on the year
+  # scale_linetype_manual(name = 'Line Type', values = c("Mean" = "dashed", "Max" = "dashed", "Min" = "dashed")) +
+  scale_x_datetime(labels = scales::date_format("%B")) +
+  # scale_fill_manual(name = 'Fill', values = c('5th to 95th\npercentile' = 'grey')) +
+  labs(y = y_lab, colour = 'Water Year') +
+  theme_bw() +
+  theme(axis.title.x = element_blank())
+
+gg_at <- ggplot(qc_fill_data)  +
+  geom_line(aes(
+    plot_time, PC_accumulated_wtr_yr,
+    colour = as.factor(year),
+    group = as.factor(year),
+  )) +
+    geom_ribbon(
+      data = hourly_stats_at |> filter(name == sel_var),
+      aes(
+        x = plot_time,
+        ymin = lower_quantile,
+        ymax = upper_quantile,
+        fill = '5th to 95th\npercentile'
+      ),
+      alpha = 0.2
+    ) + 
+  scale_x_datetime(labels = scales::date_format("%B")) +
+  labs(x = "Month", y = "Hourly Air Temperature (°C)") +
+  theme_bw()
+
+gg_at
+
+if(quant == T){
+  gg_at <- gg_at +
+  geom_line(data = hourly_stats_at,
+            aes(x = plot_time, 
+                y = upper_quantile, group = 1, linetype = 'Upper Quantile')) +
+    geom_line(data = hourly_stats_at, 
+              aes(x = plot_time, 
+                  y = lower_quantile, 
+                  group = 1, 
+                  linetype = 'Lower Quantile')) 
+} 
+
+if(means == T){
+  gg_at <- gg_at +
+    geom_line(data = hourly_stats_at, aes(
+      x = plot_time,
+      y = max,
+      group = 1,
+      linetype = 'Max'
+    )) +
+    geom_line(data = hourly_stats_at, aes(
+      x = plot_time,
+      y = mean,
+      group = 1,
+      linetype = 'Mean'
+    )) +
+    geom_line(data = hourly_stats_at, aes(
+      x = plot_time,
+      y = min,
+      group = 1,
+      linetype = 'min'
+    )) 
+} 
+gg_at
+
+plotly::ggplotly()
+
+hourly_stats_sd <- hourly_stats |> filter(name == 'Snow_Depth_qaqc_filled')
+
+ggplot(qc_fill_data)  +
+  geom_line(aes(
+    plot_time, Snow_Depth_qaqc,
     colour = as.factor(year),
     group = as.factor(year),
   ),
   alpha = ifelse(qc_fill_data$year == max_year, 1, 0.5)) +
-  # geom_line(data = glob_avg, aes(x = month_num, y = glob_max_monthly_temp, group = 1, linetype = 'Max')) +
-  # geom_line(data = glob_avg, aes(x = month_num, y = glob_mean_monthly_temp, group = 1, linetype = 'Mean')) +
-  # geom_line(data = glob_avg, aes(x = month_num, y = glob_min_monthly_temp, group = 1, linetype = 'Min')) +
-  scale_color_manual(name = 'Year', values = color_palette) +  # Assign colors based on the year
+  geom_ribbon(
+    data = hourly_stats_sd,
+    aes(
+      x = plot_time,
+      ymin = lower_quantile,
+      ymax = upper_quantile,
+      fill = '5th to 95th\npercentile'
+    ),
+    alpha = 0.2
+  ) + 
+  geom_line(data = hourly_stats_sd, aes(x = plot_time, y = upper_quantile, group = 1, linetype = 'Upper Quantile')) +
+  geom_line(data = hourly_stats_sd, aes(x = plot_time, y = lower_quantile, group = 1, linetype = 'Lower Quantile')) +
+  geom_line(data = hourly_stats_sd, aes(x = plot_time, y = max, group = 1, linetype = 'Max')) +
+  geom_line(data = hourly_stats_sd, aes(x = plot_time, y = mean, group = 1, linetype = 'Mean')) +
+  geom_line(data = hourly_stats_sd, aes(x = plot_time, y = min, group = 1, linetype = 'min')) +
+  # scale_color_manual(name = 'Year', values = color_palette) +  # Assign colors based on the year
   # scale_linetype_manual(name = 'Line Type', values = c("Mean" = "dashed", "Max" = "dashed", "Min" = "dashed")) +
   scale_x_datetime(labels = scales::date_format("%B")) +
   # scale_fill_manual(name = 'Fill', values = c('5th to 95th\npercentile' = 'grey')) +
-  labs(x = "Month", y = "Hourly Temperature (°C)") +
+  labs(x = "Month", y = "Snow Depth (cm)") +
   theme_bw()
 
 plotly::ggplotly()
--- scripts/summary-plots/run-summary-plots.R
+++ scripts/summary-plots/run-summary-plots.R
@@ -20,58 +20,57 @@ options(ggplot2.discrete.colour= c(
 ))
 
 chrl_stations <- c(
-  'apelake',
-  'mountcayley',
-  'claytonfalls',
-  'lowercain',
-  'mountarrowsmith',
-  'tetrahedron',
-  'machmellkliniklini',
-  'eastbuxton'
-  # 'datlamen', #TODO
-  # 'rennellpass', #TODO
+  # 'upperskeena',
+  # 'apelake',
+  # 'mountcayley',
+  # 'claytonfalls',
+  # 'lowercain',
+  # 'mountarrowsmith',
+  # 'tetrahedron',
+  # 'machmellkliniklini',
+  # 'eastbuxton',
+  # # 'datlamen',no met qc as of Feb 9
+  # 'rennellpass',
   # 'mountmaya',
-  # 'machmell',
+  # 'machmell',no met qc as of Feb 9
   # 'klinaklini',
-  # 'plummerhut',
-  # 'homathko',
-  # 'cainridgerun',
-  # 'steph1',
-  # 'steph2',
-  # 'steph3',
-  # 'steph4',
-  # 'steph6',
-  # 'steph7',
-  # 'steph8',
-  # 'upperrussell',
-  # 'russellmain',
-  # 'uppercruickshank',
-  # 'perseverance'
+  'plummerhut',
+  'homathko',
+  'cainridgerun',
+  'steph1',
+  'steph2',
+  'steph3',
+  'steph4',
+  'steph6',
+  'steph7',
+  'steph8',
+  'upperrussell',
+  'russellmain',
+  'uppercruickshank',
+  'perseverance'
 )
-
-conn <- do.call(DBI::dbConnect, args)
-on.exit(DBI::dbDisconnect(conn))
 
 for (stn in chrl_stations) {
   
   cur_stn <- stn # eventually loop through all stations...
   
   # load data ---
+  conn <- do.call(DBI::dbConnect, args)
+  on.exit(DBI::dbDisconnect(conn))
   source('scripts/summary-plots/00-load-data.R')
 
   # qaqc loop ---
 
   print(paste("Starting QAQC for", cur_stn))
-
-  # eventually pull from Juliens QAQCd data
-
-  source('scripts/summary-plots/01-qaqc-met-data.R')
-  source('scripts/summary-plots/02-gap-fill-met-data.R')
-
-  print(paste("Finished QAQC for", cur_stn))
-
-  # stats ----
   
+  # source('scripts/summary-plots/01-qaqc-precip-data.R') # this only handles air temp and is depreciated
+  
+  source('scripts/summary-plots/01-qaqc-precip-data.R')  # this applies a secondary qc to the raw pipe data sergey and julien created, although their qc is good there is still a bit of loss in some of the data and we need to fill gaps for the stats
+  
+  print(paste("Finished QAQC for", cur_stn))
+  # 
+  # # stats ----
+
   print(paste("Starting stats for", cur_stn))
 
   source('scripts/summary-plots/03-stats-on-met-data.R')
@@ -80,10 +79,19 @@ for (stn in chrl_stations) {
 
   # plots ----
 
-  print(paste("Starting plots for", cur_stn))
-
-  source('scripts/summary-plots/04-plot-monthly-stats.R')
-
-  paste("Finished plots for", cur_stn)
+  # print(paste("Starting plots for", cur_stn))
+  # 
+  # source('scripts/summary-plots/04-plot-monthly-stats.R')
+  # 
+  # paste("Finished plots for", cur_stn)
 
 }
+
+# Move all log files into /logs
+files <- list.files(pattern = "*.log", full.names = TRUE, recursive = F)
+
+# Move files to the log directory
+file.copy(files, "logs")
+
+# Remove original files
+file.remove(files)
